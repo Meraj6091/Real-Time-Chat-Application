fe12bc22b8f45c71f96011cac49ac249e0dd572a
meraj6091 <merajvindira2000@gmail.com>
2021-11-06 17:24:01 +0530

    socket integration

--- client/src/components/Message/message.js
+++ client/src/components/Message/message.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './message.css';
+import ReactEmoji from 'react-emoji';
 
 function Message({ message, name }) {
     const { user, text } = message;
@@ -12,13 +13,13 @@ function Message({ message, name }) {
         <div className="messageContainer justifyEnd">
             <p className="sentText pr-10"> {trimName}</p>
             <div className="messageBox backgroundBlue">
-                <p className="messageText colorWhite">{text}</p>
+                <p className="messageText colorWhite">{ReactEmoji.emojify(text)}</p>
             </div>
         </div>
     ) : (
         <div className="messageContainer justifyStart">
             <div className="messageBox backgroundLight">
-                <p className="messageText colorDark">{text}</p>
+                <p className="messageText colorDark">{ReactEmoji.emojify(text)}</p>
             </div>
             <p className="sentText pl-10"> {user}</p>
         </div>
